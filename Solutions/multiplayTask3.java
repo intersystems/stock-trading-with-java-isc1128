c6d752b80dac3a69cba00f9497bfecbcefde32ca
DuongDo-InterSystems <Duong.Do@intersystems.com>
2018-12-04 17:20:27 -0500

    Update code using config file for multiplay

--- Solutions/multiplayTask3.java
+++ Solutions/multiplayTask3.java
@@ -1,14 +1,16 @@
 /*
 * PURPOSE: Create StockInfo objects that can be manipulated
-*
-* NOTES: To use locally, change the IP and port of dbUrl to values for your
-*  instance: xepPersister.connect("YourIP",YourPort,"USER",user,pass);
 */
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 import com.intersystems.jdbc.IRIS;
 import com.intersystems.xep.Event;
@@ -22,16 +24,32 @@ import com.intersystems.jdbc.IRISConnection;
 public class multiplayTask3 {
 
 	public static void main(String[] args) {
-		String user = "SuperUser";
-		String pass = "SYS";
+		// Initialize map to store connection details from config.txt
+	    HashMap<String, String> map = new HashMap<String, String>();
+		try{
+			map = getConfig("config.txt");
+		}
+		catch (IOException e){
+			System.out.println(e.getMessage());
+		}
+
+		// Retrieve connection information
+		String host = map.get("host");
+		String port = map.get("port");
+		String namespace = map.get("namespace");
+		String username = map.get("username");
+		String password = map.get("password");
 		
 		try {
 			// Connect to database using EventPersister, which is based on IRISDataSource
 	        EventPersister xepPersister = PersisterFactory.createPersister();
-	        xepPersister.connect("127.0.0.1",51773,"User",user,pass); 
+
+	        // Connecting to database
+	        xepPersister.connect(host,port,namespace,username,password);
 	        System.out.println("Connected to InterSystems IRIS via JDBC.");
-	        xepPersister.deleteExtent("Demo.StockInfo");   // remove old test data
-	        xepPersister.importSchema("Demo.StockInfo");   // import flat schema
+
+	        xepPersister.deleteExtent("Demo.StockInfo");   // Remove old test data
+	        xepPersister.importSchema("Demo.StockInfo");   // Import flat schema
 	       
 	        //***Initializations***
 	        // Create XEP Event for object access
@@ -79,4 +97,35 @@ public class multiplayTask3 {
 		}
 	        
 	}
+
+	// Helper method: Get connection details from config file
+	public static HashMap<String, String> getConfig(String filename) throws FileNotFoundException, IOException{
+        // Initial empty map to store connection details
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        String line;
+
+        // Using Buffered Reader to read file
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+
+        while ((line = reader.readLine()) != null)
+        {
+            // Remove all spaces and split line based on first colon
+            String[] parts = line.replaceAll("\\s+","").split(":", 2);
+
+            // Check if line contains enough information
+            if (parts.length >= 2)
+            {
+                String key = parts[0];
+                String value = parts[1];
+                map.put(key, value);
+            } else {
+                System.out.println("Ignoring line: " + line);
+            }
+        }
+
+        reader.close();
+
+        return map;
+    }
 }
